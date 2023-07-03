1f60e5374945281882b02d238b0f11de0db8bb2b
Feyyaz Numan Cavlak <feyyazcavlak@outlook.com>
2023-07-03 15:10:02 +0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Dashboard, Sidebar } from "./components";
+
+function App() {
+  return (
+    <>
+      <h1>Ondokuzon Demo</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Sidebar from "./Sidebar/Sidebar";
+import Dashboard from "./Dashboard/Dashboard";
+
+export { Sidebar, Dashboard };
